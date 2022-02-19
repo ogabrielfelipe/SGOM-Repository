0be2d17f0c5f0a673730e9a546920507e7ad17e3
Gabriel <gabrielfelipe100x@gmail.com>
2022-02-19 19:51:49 -0300

    ajustes

--- app/bluePrints/ordemDeServico.py
+++ app/bluePrints/ordemDeServico.py
@@ -4,6 +4,6 @@ from flask import Blueprint, render_template
 ordem = Blueprint('ordem', __name__)
 
 
-@ordem.route('/OrdemDeServico'. methods=['GET'])
+@ordem.route('/OrdemDeServico', methods=['GET'])
 def root_ordemDeServico():
     return render_template('.html')
