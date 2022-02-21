345164952e2ec0af14726b7b6146b6498ef2622f
Gabriel Felipe Vieira <80602315+ogabrielfelipe@users.noreply.github.com>
2022-02-21 15:43:12 -0300

    Delete Consulta_OS.sql

--- Consulta_OS.sql
+++
@@ -1,8 +0,0 @@
-SELECT d.nomeRequerente, d.cpfDoRequerente, d.telefoneRequerente, d.problema,
-       d.requisicaoOrcamento, d.estadoAtualDoVeiculo, d.custoMecanico, d.valorTodal,
-       d.respostaCliente, c.placa, f.nome
-FROM ordemDeServico AS d
-INNER JOIN carro c on c.id = d.id
-INNER JOIN funcionario f on f.id = d.mecanico
-WHERE
-    D.id = 2
