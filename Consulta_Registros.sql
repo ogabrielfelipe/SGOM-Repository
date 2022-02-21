8c6b3599caa8e3e6799fa44bfb5422ddc3c5a078
Gabriel Felipe Vieira <80602315+ogabrielfelipe@users.noreply.github.com>
2022-02-21 15:43:18 -0300

    Delete Consulta_Registros.sql

--- Consulta_Registros.sql
+++
@@ -1,5 +0,0 @@
-SELECT r.data, r.novoStatus, f.nome, r.problema  FROM ordemDeServico AS o
-INNER JOIN registroDaOS r ON r.ordemServico = o.id
-INNER JOIN funcionario AS f ON f.id = o.mecanico
-WHERE
-    o.id = 2
