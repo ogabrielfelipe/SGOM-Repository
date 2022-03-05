639524298a49eff015f55641fb3f1e6fc3bb80c1
Gabriel <gabrielfelipe100x@gmail.com>
2022-03-05 16:07:59 -0300

    ajustes

--- app/bluePrints/login/auth.py
+++ app/bluePrints/login/auth.py
@@ -2,15 +2,7 @@ from flask import Blueprint, request, render_template, redirect, url_for, jsonif
 from ...controller import (
     funcionarioController
 )
-from flask_jwt_extended import (
-    create_access_token,
-    get_jwt,
-    JWTManager,
-    jwt_required,
-    get_jwt_identity,
-    set_access_cookies,
-    unset_jwt_cookies
-)
+
 from flask_login import login_required, logout_user
 
 
